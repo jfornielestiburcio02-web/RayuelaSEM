8fe82664b429400aec07e52a1042e75b83e8d3ee
juan fornieles <jfornielestiburcio02@gmail.com>
2025-12-19 18:08:46 +0000

    (PUedes notificar dias futuros  al de hoy)

--- src/app/modulo_acceso/Controlador/JustificarAusencias.tsx
+++ src/app/modulo_acceso/Controlador/JustificarAusencias.tsx
@@ -149,7 +149,7 @@ export default function JustificarAusencias() {
                                                             mode="single"
                                                             selected={field.value}
                                                             onSelect={field.onChange}
-                                                            disabled={(date) => date > new Date() || date < new Date("1900-01-01")}
+                                                            disabled={(date) => date < new Date("1900-01-01")}
                                                             initialFocus
                                                         />
                                                     </PopoverContent>
@@ -184,7 +184,7 @@ export default function JustificarAusencias() {
                                                             mode="single"
                                                             selected={field.value}
                                                             onSelect={field.onChange}
-                                                            disabled={(date) => date > new Date() || date < new Date("1900-01-01")}
+                                                            disabled={(date) => date < new Date("1900-01-01")}
                                                             initialFocus
                                                         />
                                                     </PopoverContent>
