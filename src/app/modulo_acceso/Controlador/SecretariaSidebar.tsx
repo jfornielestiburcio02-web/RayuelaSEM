df2ae1c5756b337baeb0fc40973bf7ba22c47ca2
juan fornieles <jfornielestiburcio02@gmail.com>
2025-12-19 09:50:15 +0000

    LA IMAGEN DE DENTRO COÑO NOO LOS TRAMITES ME CAGO EN TI

--- src/app/modulo_acceso/Controlador/SecretariaSidebar.tsx
+++ src/app/modulo_acceso/Controlador/SecretariaSidebar.tsx
@@ -2,7 +2,6 @@
 
 import { Button } from "@/components/ui/button";
 import { FileText } from "lucide-react";
-import Image from "next/image";
 
 type SecretariaSidebarProps = {
     onSelectOption: (option: 'tramites') => void;
@@ -10,16 +9,7 @@ type SecretariaSidebarProps = {
 
 export default function SecretariaSidebar({ onSelectOption }: SecretariaSidebarProps) {
   return (
-    <div className="w-64 bg-gray-100 dark:bg-gray-800 p-4 flex flex-col space-y-4 shadow-md">
-        <div className="flex justify-center mb-4">
-            <Image
-              src="https://i.ibb.co/gZvGkphp/Dise-o-sin-t-tulo-14-1.png"
-              alt="Logo Secretaría"
-              width={150}
-              height={100}
-              data-ai-hint="logo"
-            />
-        </div>
+    <div className="w-64 bg-gray-100 dark:bg-gray-800 p-4 flex flex-col space-y-4 shadow-md pt-8">
 
       <Button variant="ghost" className="justify-start gap-2" onClick={() => onSelectOption('tramites')}>
         <FileText className="h-5 w-5" />
