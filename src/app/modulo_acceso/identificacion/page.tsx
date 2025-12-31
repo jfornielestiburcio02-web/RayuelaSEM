4e51afb32dab4e6931c9727cfbbc6cf21541841a
jfornielestiburcio02-web <jfornielestiburcio02@gmail.com>
2025-12-31 18:15:45 +0100

    Update page.tsx

--- src/app/modulo_acceso/identificacion/page.tsx
+++ src/app/modulo_acceso/identificacion/page.tsx
@@ -1,11 +1,10 @@
 export default function SystemDown() {
   return (
     <>
-      <h1>⚠️ SISTEMA HA CAÍDO ⚠️</h1>
+      <h1> SISTEMA HA CAÍDO </h1>
       <p>Se ha detectado un fallo crítico en la infraestructura.</p>
       <p>Todos los servicios están inoperativos.</p>
       <p>No intente reiniciar el sistema.</p>
-      <p>Su información podría estar comprometida.</p>
       <p>El personal técnico ha sido notificado.</p>
     </>
   );
