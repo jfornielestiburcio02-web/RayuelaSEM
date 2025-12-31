32514bb9d07008ab5fb3718b2113143c5fff8280
jfornielestiburcio02-web <jfornielestiburcio02@gmail.com>
2025-12-31 18:28:44 +0100

    Update page.tsx

--- src/app/modulo_acceso/identificacion/page.tsx
+++ src/app/modulo_acceso/identificacion/page.tsx
@@ -1,7 +1,11 @@
 export default function SystemDown() {
   return (
     <>
-      <h1> El servidor ha sufrido un error. Servicio Inhabilitado temporalmente. Estamos trabajando para restablecer el servicio.  </h1>
+      <h1>  SISTEMA HA CAÍDO </h1>
+<p1> Se ha detectado un fallo crítico en la infraestructura.</p1>
+ </p1> Todos los servicios están inoperativos. </p1>
+</p1>No intente reiniciar el sistema.</p1>
+</p1>El personal técnico ha sido notificado.</p1>
       <p1> Att: doctor4446666 </p1>
     </>
   );
