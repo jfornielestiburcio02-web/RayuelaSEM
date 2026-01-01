5b1c2f05390db22b7eb7dc48f8e68e228f7c0dbb
jfornielestiburcio02-web <jfornielestiburcio02@gmail.com>
2026-01-01 11:38:07 +0100

    Update page.tsx

--- src/app/modulo_acceso/identificacion/page.tsx
+++ src/app/modulo_acceso/identificacion/page.tsx
@@ -3,6 +3,8 @@ export default function SystemDown() {
     <>
       <p>El servidor está destruido.</p>
       <p>Estamos trabajando para restablecer el servicio.</p>
+      <h1>Código de error: 7091</h1>
+      <p3>  ErrorOutputElse:;`MODULOSELECCIONADOCARGAR.js</p3>
     </>
   );
 }
